40355c5e02c0b4a0ac45fcb500203451007d4dc7
Armand Dupuis <armand.dupuis@newthings.co>
2022-02-23 23:09:58 +0200

    feat: apply grayscale transform to image

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
+use base64::decode;
+use image::{load_from_memory, DynamicImage, ImageOutputFormat, ImageResult};
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded: &str) {
-    log(&encoded.into());
+    let decoded = decode(encoded).unwrap();
+    let image = load_from_memory(&decoded).unwrap();
+    log(&"Imaged parsed!".into());
+
+    let grayscale = image.grayscale();
+    log(&"Grayscale applied".into());
+
+    let mut buffer = vec![];
+
+    grayscale
+        .write_to(&mut buffer, ImageOutputFormat::Png)
+        .unwrap();
+
+    log(&"image to buffer".into());
 }
 
 #[cfg(test)]
