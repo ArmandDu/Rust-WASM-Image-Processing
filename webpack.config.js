61d23429958fb4baff47bffafd6f3df4e57abce7
Armand Dupuis <armand.dupuis@newthings.co>
2022-02-23 22:30:58 +0200

    chore: add wasm-bindgen and wasm-pack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-const path = require("path")
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WasmWebpackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-    entry: "./web/main.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js"
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./web/index.html"
-    })]
-}
+  entry: "./web/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./web/index.html",
+    }),
+    new WasmWebpackPlugin({
+      crateDirectory: path.resolve(__dirname, "."),
+    }),
+  ],
+};
